ed59c56dc8d8c9a76dff57f9885f0b9e3b65e45f
pedram <pedramalizade03@gmail.com>
2026-01-14 21:52:28 +0330

    fix some problems

--- KareMa.Domain.AppService/obj/Debug/net9.0/KareMa.Domain.AppService.AssemblyInfo.cs
+++ KareMa.Domain.AppService/obj/Debug/net9.0/KareMa.Domain.AppService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KareMa.Domain.AppService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402bc7ffd2060be542ada27cedf9a5c51193b074")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b9863a3975bea708f8f0d67fbe8da849b05754")]
 [assembly: System.Reflection.AssemblyProductAttribute("KareMa.Domain.AppService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KareMa.Domain.AppService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
